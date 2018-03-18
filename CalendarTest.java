586c5070ce9c65167d9ff86ed5b033d7dfee0e7f
D-DO <37467092+D-DO@users.noreply.github.com>
2018-03-18 21:44:40 +0700

    Add result

--- CalendarTest.java
+++ CalendarTest.java
@@ -3,15 +3,17 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class CalendarTest {
-    public static void main(String[] args) {
-            Calendar cal = new GregorianCalendar();
-            cal.set(Calendar.YEAR, 2011);
-            cal.set(Calendar.MONTH, Calendar.APRIL);
-            cal.set(Calendar.DAY_OF_MONTH, 30);
-            cal.add(Calendar.DAY_OF_MONTH, 1);
-            cal.roll(Calendar.MONTH, 11);
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy MMMM dd");
-            System.out.println(sdf.format(cal.getTime()));
-        }
-
+        public static void main(String[] args) {
+        Calendar cal = new GregorianCalendar();
+        cal.set(Calendar.YEAR, 2011);
+        cal.set(Calendar.MONTH, Calendar.APRIL);
+        cal.set(Calendar.DAY_OF_MONTH, 30);
+        cal.add(Calendar.DAY_OF_MONTH, 1);
+        cal.roll(Calendar.MONTH, 11);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy MMMM dd");
+        System.out.println(sdf.format(cal.getTime()));
     }
+
+}
+
+//2011 апреля 01
